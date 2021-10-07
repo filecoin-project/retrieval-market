926ff8de2c04110ebf28c108837fe5038925768e
patrickwoodhead <patrick.woodhead@protocol.ai>
2021-10-07 11:24:57 +0100

    exportPathMap added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   trailingSlash: true,
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/indexing": { page: "/indexing" },
+      "/data-transfer": { page: "/data-transfer" },
+      "/reputation-systems": { page: "/reputation-systems" },
+      "/payment-channels": { page: "/payment-channels" },
+    };
+  },
 };
