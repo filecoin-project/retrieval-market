81a0c51d12c1a8ad09a84edb45aaee294d0bdff5
patrickwoodhead <patrick.woodhead@protocol.ai>
2021-10-18 12:20:51 +0100

    demo day meeting link

--- components/home/Events.tsx
+++ components/home/Events.tsx
@@ -4,9 +4,21 @@ export default function Events() {
   return (
     <PanelWrapper title="Events">
       <h3 className="text-1xl text-left pt-2">
-        There is nothing in the diary right now, but we are hoping to run
-        another summit before the end of the year. Keep checking this panel for
-        updates!
+        Please join the{" "}
+        <a
+          className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+          href="https://calendar.google.com/event?action=TEMPLATE&tmeid=XzhwMmplZTFtNmtzM2NiOW02Z3JqYWI5azZrcGthYmEyOHAzNDRiYTM4a3MzaWhobTZvcGpnYzI1NjRfMjAyMTEwMjdUMjAwMDAwWiBwYXRyaWNrLndvb2RoZWFkQHByb3RvY29sLmFp&tmsrc=patrick.woodhead%40protocol.ai&scp=ALL"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Retrieval Market Builders Demo Day Meeting
+        </a>{" "}
+        which happens every other week on a Wednesday.
+      </h3>
+      <h3 className="text-1xl text-left pt-2">
+        Aprt from that, there is currently nothing more in the diary right now,
+        but we are hoping to run another summit before the end of the year. Keep
+        checking this panel for updates!
       </h3>
     </PanelWrapper>
   );
