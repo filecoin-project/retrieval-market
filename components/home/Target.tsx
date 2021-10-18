fee91b21861c0ef75ad1e715c07abb5716f950b0
patrickwoodhead <patrick.woodhead@protocol.ai>
2021-10-18 10:45:50 +0100

    node to retrieval provider

--- components/home/Target.tsx
+++ components/home/Target.tsx
@@ -5,8 +5,8 @@ export default function Target() {
   return (
     <PanelWrapper title="Target">
       <h3 className="text-1xl pt-4">
-        🎯 The Filecoin Retrieval Market launches with 1,000 nodes and
-        sub-second delivery of content world-wide 🎯
+        🎯 The Filecoin Retrieval Market launches with 1,000 retrieval providers
+        and sub-second delivery of content world-wide 🎯
       </h3>
       <div className="flex flex-wrap items-center justify-center px-12 max-w-4xl sm:w-full">
         <h2 className="flex flex-wrap items-center justify-center px-12 py-8 max-w-4xl sm:w-full">
